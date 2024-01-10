c7a574b818ff28ed09e58079f1946e38ca0ed096
Mesi <mse212002@gmail.com>
2024-01-10 10:34:10 +0200

    :sparkles: add last_five_comments method to the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   has_many :comments
   has_many :likes
+  after_save :update_post_counter
+
+  def last_five_comments
+    comments.order(created_at: :DESC).limit(5)
+  end
+
+  private
+  def update_post_counter
+    user.update(posts_counter: user.posts.length)
+  end
 end
