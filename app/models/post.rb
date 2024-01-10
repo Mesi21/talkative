c92d624cf4cd263bc2b65868ccf9a2df371056d4
Mesi <mse212002@gmail.com>
2024-01-10 10:54:50 +0200

    :construction: update method name in post model to update_posts_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,14 +2,15 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   has_many :comments
   has_many :likes
-  after_save :update_post_counter
+  after_save :update_posts_counter
 
   def last_five_comments
     comments.order(created_at: :DESC).limit(5)
   end
 
   private
-  def update_post_counter
-    user.update(posts_counter: user.posts.length)
+
+  def update_posts_counter
+    user.update(posts_counter: user.posts.count)
   end
 end
