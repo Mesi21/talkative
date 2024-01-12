13de57e4865d9b18b926aa334ab73e240d2e8276
Mesi <mse212002@gmail.com>
2024-01-12 22:33:06 +0200

    :white_check_mark: add test for validations of the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: {in 1..250}
+  validates :title, presence: true, length: { in: 1..250 }
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
   validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
   belongs_to :author, class_name: 'User', foreign_key: :author_id
@@ -11,8 +11,8 @@ class Post < ApplicationRecord
     comments.order(created_at: :DESC).limit(5)
   end
 
-  private
-
+  private 
+  
   def update_posts_counter
     author.update(posts_counter: author.posts.length)
   end
