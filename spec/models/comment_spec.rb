6b81fa5c5cfc1601b56e0abeb102af8ce37772de
Mesi <mse212002@gmail.com>
2024-01-13 12:27:43 +0200

    :rotating_light: fix linter errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,10 +1,13 @@
 require './spec/rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:current_user) {User.new(name: 'Mesi', photo: 'https://unsplash.com/photos/man-in-front-of-laptop-computer-in-shallow-focus-photography-LNRyGwIJr5c', bio: 'Web developer from Romania.', posts_counter: 1)}
-  let(:current_post) {Post.new(title: 'Module restart and frustrations', text: 'test', author: current_user, comments_counter: 8, likes_counter: 12)}
+  let(:current_user) { User.new(name: 'Mesi', photo: 'https://unsplash.com/photos/man-in-front-of-laptop-computer-in-shallow-focus-photography-LNRyGwIJr5c', bio: 'Web developer from Romania.', posts_counter: 1) }
+  let(:current_post) do
+    Post.new(title: 'Module restart and frustrations', text: 'test', author: current_user, comments_counter: 8,
+             likes_counter: 12)
+  end
   subject { Comment.new(text: 'test a comment', user: current_user, post: current_post) }
-  
+
   it 'should validate user with all elements filled' do
     expect(subject).to be_valid
   end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,9 +1,12 @@
 require './spec/rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:current_user) {User.new(name: 'Mesi', photo: 'https://unsplash.com/photos/man-in-front-of-laptop-computer-in-shallow-focus-photography-LNRyGwIJr5c', bio: 'Web developer from Romania.', posts_counter: 1)}
-  let(:current_post) {Post.new(title: 'Module restart and frustrations', text: 'test', author: current_user, comments_counter: 8, likes_counter: 12)}
-  subject {Like.new(user: current_user, post: current_post)}
+  let(:current_user) { User.new(name: 'Mesi', photo: 'https://unsplash.com/photos/man-in-front-of-laptop-computer-in-shallow-focus-photography-LNRyGwIJr5c', bio: 'Web developer from Romania.', posts_counter: 1) }
+  let(:current_post) do
+    Post.new(title: 'Module restart and frustrations', text: 'test', author: current_user, comments_counter: 8,
+             likes_counter: 12)
+  end
+  subject { Like.new(user: current_user, post: current_post) }
 
   it 'should be valid with proper user and post' do
     expect(subject).to be_valid
