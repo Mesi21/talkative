b4032ba07c3d2bb749b41675b32f84e4c7189651
Mesi <mse212002@gmail.com>
2024-01-15 10:06:03 +0200

    :white_check_mark: add test to check the response status, template and placeholder text of the posts controller index action\'s result

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+
+    before(:each) do
+      @user = User.new(name: 'Mesi', posts_counter: 5)
+      @user.save
+      get '/users/1/posts'
+    end
+    it 'checks the correct response status, template and placeholder text' do
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('index')
+      expect(response.body).to include('for all posts')
+    end
   end
 end
