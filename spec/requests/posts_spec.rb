909725ca0545bdaa7ed0cadbabc07c0a6c0656dd
Mesi <mse212002@gmail.com>
2024-01-15 10:08:53 +0200

    :white_check_mark: add test to check the response status, template and placeholder text of the posts controller show action\'s result

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-
     before(:each) do
       @user = User.new(name: 'Mesi', posts_counter: 5)
       @user.save
@@ -12,6 +11,23 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to have_http_status(200)
       expect(response).to render_template('index')
       expect(response.body).to include('for all posts')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) do
+      @user = User.new(name: 'Mesi', posts_counter: 5)
+      @post = Post.new(title: 'Test post', text: 'To test the posts_controller', comments_counter: 2, likes_counter: 5,
+                       author_id: 1)
+      @user.save
+      @post.save
+    end
+
+    it 'shows the specific of one post' do
+      get '/users/1/posts/1'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('show')
+      expect(response.body).to include('one particular post')
     end
   end
 end
