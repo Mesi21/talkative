8ac16cc09466a5026f929a5f17faff7486ea8a52
Mesi <mse212002@gmail.com>
2024-01-15 09:37:56 +0200

    :heavy_plus_sign: add gem 'rails-controller-testing' and :white_check_mark: add test to check if the correct template is rendered

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Users', type: :request do
     it 'should render the index page with every existing user' do
       get '/users'
       expect(response).to have_http_status(200)
+      expect(response).to render_template('index')
     end
   end
 end
