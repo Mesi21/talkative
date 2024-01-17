dd30b285f6313277d6249e361b96972ba4201e38
Mesi <mse212002@gmail.com>
2024-01-17 10:02:05 +0200

    :art: add content to users index and show view files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+15.times do
+  User.create(name: Faker::Name.name, photo: Faker::Avatar.unique.image, bio: Faker::Lorem.paragraph(sentence_count: 5), posts_counter: 0)
+end
+
+50.times do
+  Post.create(author:User.find(Faker::Number.between(from: 1, to: 30)), title: Faker::Lorem.word, text: Faker::Lorem.paragraph_by_chars, comments_counter: 0, likes_counter: 0)
+end
+
+200.times do
+  Comment.create(post_id: Faker::Number.between(from: 1, to: 30), text:Faker::Lorem.sentence)
+end
+
+350.times do
+  Like.create(post_id: Faker::Number.between(from: 1, to: 30))
+end
