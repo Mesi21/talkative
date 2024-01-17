b2c3026ed87059fd5c8da68b356aa4f7706091c1
Mesi <mse212002@gmail.com>
2024-01-17 10:01:15 +0200

    :sparkles: fill methods index and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
-  def index; end
-  def show; end
+  def index
+    @user = User.find(params[:author_id])
+    @posts = @user.last_three_posts
+  end
+  def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+  end
 end
