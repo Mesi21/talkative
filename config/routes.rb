d89d893ab36dcace9277e3c8d547414ee4489697
Mesi <mse212002@gmail.com>
2024-01-19 15:35:55 +0200

    :bug: change the order of the methods inside the resources block of the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :new, :create, :show]
   end
 
   resources :posts do
