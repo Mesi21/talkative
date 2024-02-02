998e3b26496972bf08f07431661956a6e7bec8ee
Mesi <mse212002@gmail.com>
2024-02-02 22:25:45 +0200

    :white_check_mark: add test to redirect path of the see post button

--- spec/views/users_show_spec.rb
+++ spec/views/users_show_spec.rb
@@ -30,10 +30,24 @@ RSpec.feature 'Users Show', type: :feature do
     expect(page).to have_css('img[src="https://unsplash.com/photos/a-group-of-people-holding-a-teddy-bear-together-FAqkG14YOKM"]')
   end
 
-  it 'displays the user\'s last three posts' do
+  it 'displays ownly the user\'s last three posts' do
     first_user.last_three_posts.each do |_p|
       expect(page).to have_content(post4.text)
+      expect(page).to have_content(post3.text)
+      expect(page).to have_content(post2.text)
       expect(page).to_not have_content(post1.text)
     end
   end
+
+  it 'redirects to the post show page when you click on the button See all posts' do
+    first('#btn-view-post').click
+    expect(current_path).to eq(user_post_path(first_user, first_user.last_three_posts.first))
+  end
+
+  # it 'redirects to the users posts index page when you click on "See all posts"' do
+  #   click_link 'See All Posts'
+  #   expected_path = user_posts_path(user).chomp('/')
+  #   actual_path = page.current_path.chomp('/')
+  #   expect(actual_path).to eq(expected_path)
+  # end
 end
