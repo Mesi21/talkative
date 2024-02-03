ad7de9f2f50530931da670288ffce932634a1c67
Mesi <mse212002@gmail.com>
2024-02-03 22:55:23 +0200

    :sparkles: add proper authentication method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :current_user
+  def after_sign_in_path_for(_resource)
+    users_path
+  end
+  
   protected
 
   def update_allowed_parameters
