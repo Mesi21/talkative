63101a2e3af4514c16d2c083427e00c7b70a7b92
Mesi <mse212002@gmail.com>
2024-02-03 14:13:46 +0200

    :white_check_mark: add test to assert that the button to view all the posts is present on the page

--- spec/views/users_show_spec.rb
+++ spec/views/users_show_spec.rb
@@ -90,3 +90,29 @@ RSpec.feature 'Users Show', type: :feature do
     end
   end
 end
+
+
+RSpec.feature 'Users Show', type: :feature do
+  let(:first_user) { User.create!(name: 'Mesi', photo: 'https://unsplash.com/photos/a-group-of-people-holding-a-teddy-bear-together-FAqkG14YOKM', bio: 'Mesi\'s biography', posts_counter: 3) }
+  let!(:post1) do
+    Post.create!(author: first_user, title: 'Post #1', text: 'some text', comments_counter: 2, likes_counter: 1)
+  end
+  let!(:post2) do
+    Post.create!(author: first_user, title: 'Post #2', text: 'another post content', comments_counter: 2,
+                 likes_counter: 1)
+  end
+  let!(:post3) do
+    Post.create!(author: first_user, title: 'Post #3', text: 'a new post with some interesting stuff',
+                 comments_counter: 3, likes_counter: 0)
+  end
+  let!(:post4) do
+    Post.create!(author: first_user, title: 'Post #4', text: 'a post that should not be present on this page',
+                 comments_counter: 5, likes_counter: 10)
+  end
+  before do
+    visit user_path(first_user)
+  end
+  it 'checks if the button to see all the posts exists on the page' do
+    expect(page).to have_selector(:link_or_button, 'View more posts')
+  end
+end
